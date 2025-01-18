adefad00976b728043f3c24f289fc78d005fe6e4
yaswanth <82691830+yaswanthkumarch@users.noreply.github.com>
2025-01-18 19:46:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Summary from './components/Summary'; // Importing the Summary component
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the Transaction Summary</h1>
-      <Summary /> {/* Include the Summary component here */}
+
+
+    
+      <h1>Welcome to the Transaction Management </h1>
+      
+    <Summary /> {/* Include the Summary component here */}
     </div>
   );
 }
